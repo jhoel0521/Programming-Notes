3d12575fd5d17f446ddc7f4f3b1cec12041ae5a9
jhoel0521 <jhoel0521@gmail.com>
2024-12-11 10:28:45 -0400

    Nodo

---
+++ ArbolBinario/Nodo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArbolBinario
+{
+    unsafe public struct Nodo<T> : IComparable<T> where T : IComparable<T>
+    {
+        public T Valor { get; set; }
+        public Nodo<T>* Izquierdo { get; set; }
+        public Nodo<T>* Derecho { get; set; }
+
+        public Nodo(T valor)
+        {
+            Valor = valor;
+            Izquierdo = null;
+            Derecho = null;
+        }
+
+        public int CompareTo(T other)
+        {
+            return Valor.CompareTo(other);
+        }
+    }
+}
